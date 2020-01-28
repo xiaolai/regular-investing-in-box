6d29c703f17354560b9872b76ff88d13115c6e75
xiaolai <xiaolaiapple@gmail.com>
2020-01-29 03:04:52 +0800

    add a gray base line to the figure

--- data/visualization.py
+++ data/visualization.py
@@ -41,17 +41,22 @@ eos_price_change = []
 xin_price_change = []
 box_price_change = []
 ri_box_change = []
+base = []
+
 for i in range(0, number_of_rows):
     btc_price_change.append(float(sub(r"[^\d.]", "", series.at[i, "BTC Price"]))/float(sub(r"[^\d.]", "", series.at[0, "BTC Price"])) - 1)
     eos_price_change.append(float(sub(r"[^\d.]", "", series.at[i, "EOS Price"]))/float(sub(r"[^\d.]", "", series.at[0, "EOS Price"])) - 1)
     xin_price_change.append(float(sub(r"[^\d.]", "", series.at[i, "XIN Price"]))/float(sub(r"[^\d.]", "", series.at[0, "XIN Price"])) - 1)
     box_price_change.append(float(sub(r"[^\d.]", "", series.at[i, "BOX Price"]))/float(sub(r"[^\d.]", "", series.at[0, "BOX Price"])) - 1)    
     ri_box_change.append(series.at[i, "Value Accumulated"]/series.at[i, "Total Invested"] - 1)
+    base.append(0)
+
 series["BTC"] = btc_price_change
 series["EOS"] = eos_price_change
 series["XIN"] = xin_price_change
 series["BOX"] = box_price_change
 series["RI-BOX"] = ri_box_change
+series["Base"] = base
 
 # draw the figure
 ax = plt.gca()
@@ -61,6 +66,7 @@ series.plot(kind='line', x='Date', y='EOS', ax=ax, figsize = (20,10), color="bro
 series.plot(kind='line', x='Date', y='XIN', ax=ax, figsize = (20,10), color="purple")
 series.plot(kind='line', x='Date', y='BOX', ax=ax, figsize = (20,10), color="green")
 series.plot(kind='line', x='Date', y='RI-BOX', ax=ax, figsize = (20,10), color="blue")
+series.plot(kind='line', linestyle='dotted', x='Date', y='Base', ax=ax, figsize = (20,10), color="gray")
 
 # plt.show()
 plt.savefig("box-historical-price-change.png", transparent=True)
