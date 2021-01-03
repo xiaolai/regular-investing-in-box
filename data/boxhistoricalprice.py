3d700eab3a37fb9476cee0ec4a9b2c0cd9d43c34
xiaolai <lixiaolai@gmail.com>
2021-01-03 14:46:44 +0800

    updated

--- data/boxhistoricalprice.py
+++ data/boxhistoricalprice.py
@@ -1,31 +1,46 @@
 from datetime import date
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
+# from re import sub
+
+# todaysdate = date.today().strftime('%Y-%m-%d')
+
+# URL = 'https://coinmarketcap.com/currencies/bitcoin/markets/'
+# page = requests.get(URL)
+# soup = BeautifulSoup(page.content, 'html.parser')
+# results = soup.find('span', class_='cmc-details-panel-price__price')
+# for result in results:
+#     btc_price = result
+    
+# URL = 'https://coinmarketcap.com/currencies/eos/markets/'
+# page = requests.get(URL)
+# soup = BeautifulSoup(page.content, 'html.parser')
+# results = soup.find('span', class_='cmc-details-panel-price__price')
+# for result in results:
+#     eos_price = result
+
+# URL = 'https://coinmarketcap.com/currencies/mixin/markets/'
+# page = requests.get(URL)
+# soup = BeautifulSoup(page.content, 'html.parser')
+# results = soup.find('span', class_='cmc-details-panel-price__price')
+# for result in results:
+#     xin_price = result
+
 from re import sub
+import requests
+import json
 
 todaysdate = date.today().strftime('%Y-%m-%d')
 
-URL = 'https://coinmarketcap.com/currencies/bitcoin/markets/'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find('span', class_='cmc-details-panel-price__price')
-for result in results:
-    btc_price = result
-    
-URL = 'https://coinmarketcap.com/currencies/eos/markets/'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find('span', class_='cmc-details-panel-price__price')
-for result in results:
-    eos_price = result
+BTCresponse = json.loads(requests.get("https://big.one/api/v3/asset_pairs/BTC-USDT/ticker").text)
+btc_price = '${:,.2f}'.format(float(BTCresponse['data']['close']))
 
-URL = 'https://coinmarketcap.com/currencies/mixin/markets/'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find('span', class_='cmc-details-panel-price__price')
-for result in results:
-    xin_price = result
-    
+EOSresponse = json.loads(requests.get("https://big.one/api/v3/asset_pairs/EOS-USDT/ticker").text)
+eos_price = '${:,.2f}'.format(float(EOSresponse['data']['close']))
+
+XINresponse = json.loads(requests.get("https://big.one/api/v3/asset_pairs/XIN-USDT/ticker").text)
+xin_price = '${:,.2f}'.format(float(XINresponse['data']['close']))
+
 box_price = '${:,.2f}'.format((float(sub(r'[^\d.]', '', btc_price)) + float(sub(r'[^\d.]', '', eos_price)) * 1500 + float(sub(r'[^\d.]', '', xin_price)) * 8)/10000)
 0
 f = open("/root/regular-investing-in-box/data/box_price_history.txt", "a")
